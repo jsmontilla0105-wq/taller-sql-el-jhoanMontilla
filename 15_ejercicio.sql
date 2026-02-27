dd34b7634d8cbf6472adbb01e410ce062babf827
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:47:57 -0500

    Update 15_ejercicio.sql

--- 15_ejercicio.sql
+++ 15_ejercicio.sql
@@ -4,3 +4,6 @@
 SELECT MIN(precio) AS accesorio_mas_barato
 FROM productos
 WHERE categoria = 'Accesorios';
+
+-- Se obtiene el precio mínimo dentro de la categoría Accesorios.
+-- Resultado: el accesorio más barato disponible tiene un precio de 150.
