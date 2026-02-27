a1064907f05abc87b7a642166c030672ca3da64f
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:32:23 -0500

    Update 12_ejercicio.sql

--- 12_ejercicio.sql
+++ 12_ejercicio.sql
@@ -3,3 +3,5 @@
 
 SELECT AVG(precio) AS precio_promedio
 FROM productos;
+-- Se calcula el precio promedio de los productos registrados.
+-- Resultado: el precio promedio es 1,591.33 aproximadamente.
