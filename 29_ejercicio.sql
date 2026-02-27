2a9daf7031ac0344257f4f1e53d9ef4bdafbd6c3
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:53:42 -0500

    Update 29_ejercicio.sql

--- 29_ejercicio.sql
+++ 29_ejercicio.sql
@@ -5,3 +5,7 @@ SELECT
     (SUM(CASE WHEN cupon_usado = 'ULTIMO_SUSPIRO' THEN total_venta ELSE 0 END) * 100.0
      / SUM(total_venta)) AS porcentaje_cupon
 FROM ventas;
+
+
+-- Se calcula el porcentaje del total de ingresos generado por el cupón ULTIMO_SUSPIRO.
+-- Resultado: el cupón representa aproximadamente el 15.03% del total de ingresos.
