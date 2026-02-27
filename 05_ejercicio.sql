38c9d20d727f03a9f28704262d006fed9c16acc8
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:10:27 -0500

    Update 05_ejercicio.sql

--- 05_ejercicio.sql
+++ 05_ejercicio.sql
@@ -4,3 +4,6 @@
 SELECT nombre, correo, fecha_nacimiento
 FROM clientes
 ORDER BY nombre ASC;
+
+-- Se consultan los campos nombre, correo y fecha_nacimiento de la tabla clientes.
+-- Resultado: se muestra la lista completa de clientes ordenada alfabéticamente por nombre.
