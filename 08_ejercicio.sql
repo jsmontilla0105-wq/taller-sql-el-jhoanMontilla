f1287921269c39ed781e77b712e99a630143879b
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:20:40 -0500

    Update 08_ejercicio.sql

--- 08_ejercicio.sql
+++ 08_ejercicio.sql
@@ -4,3 +4,7 @@
 SELECT COUNT(*) AS ventas_con_cupon
 FROM ventas
 WHERE cupon_usado = 'ULTIMO_SUSPIRO';
+
+
+-- Se cuenta la cantidad de ventas en las que se utilizó el cupón "ULTIMO_SUSPIRO".
+-- Resultado: se registraron 250
