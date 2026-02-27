b2a77c8d21a29ae1e5887fae42722c6e02017ce0
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:49:36 -0500

    Update 18_ejercicio.sql

--- 18_ejercicio.sql
+++ 18_ejercicio.sql
@@ -9,3 +9,6 @@ FROM clientes c
 JOIN ventas v ON c.id_cliente = v.id_cliente
 WHERE (strftime('%Y', 'now') - strftime('%Y', c.fecha_nacimiento)) >= 60
 GROUP BY c.id_cliente;
+
+-- Se identifican los clientes mayores de 60 años que han realizado compras.
+-- Resultado: se muestra el nombre, edad aproximada y el total gastado por cada cliente mayor de 60.
