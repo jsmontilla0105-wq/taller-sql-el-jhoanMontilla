12b4af965ebefb3ba7a50c426f9845e12c99e7d5
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:31:13 -0500

    Update 10_ejercicio.sql

--- 10_ejercicio.sql
+++ 10_ejercicio.sql
@@ -5,3 +5,6 @@ SELECT nombre, correo, fecha_nacimiento
 FROM clientes
 WHERE genero = 'F'
 ORDER BY fecha_nacimiento ASC;
+-- Se consultan las clientas de género femenino.
+-- Resultado: se muestra una tabla con nombre, correo y fecha de nacimiento,
+-- ordenadas desde las más antiguas hasta las más recientes.
