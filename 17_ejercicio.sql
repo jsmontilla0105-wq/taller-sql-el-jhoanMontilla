7cb3a53bd564a22b14f9743df3262ee409ecf74e
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:48:46 -0500

    Update 17_ejercicio.sql

--- 17_ejercicio.sql
+++ 17_ejercicio.sql
@@ -4,3 +4,6 @@
 SELECT SUM((total_venta / 0.70) - total_venta) AS dinero_no_ingresado
 FROM ventas
 WHERE cupon_usado = 'ULTIMO_SUSPIRO';
+
+-- Se calcula el dinero que dejó de ingresar la empresa por el uso del cupón ULTIMO_SUSPIRO.
+-- Resultado: el monto total no ingresado por descuentos es 359,385.
