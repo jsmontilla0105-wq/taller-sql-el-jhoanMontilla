34af6f2d867bcea8f32c3b968472b071ba207367
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:18:30 -0500

    Update 07_ejercicio.sql

--- 07_ejercicio.sql
+++ 07_ejercicio.sql
@@ -5,3 +5,11 @@ SELECT nombre, precio
 FROM productos
 WHERE categoria = 'Accesorios'
   AND precio < 500;
+
+-- Se consultan los productos de la categoría "Accesorios" cuyo precio sea menor a 500.
+-- Resultado: se muestra una lista con el nombre y precio de los accesorios que cumplen esa condición.
+--"Casco Abierto Ligero"	"450"
+--"Altímetro Digital Precisión"	    "280"
+--"Altímetro Analógico Confiable"	  "150"
+--"Gafas Panorámicas Anti-reflejo"	"320"
+--"Guantes Térmicos Profesionales" 	"240"
