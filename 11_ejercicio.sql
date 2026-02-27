bd5bdd8778fcca944bdcfdea853ec67dd5d79cc7
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:31:55 -0500

    Update 11_ejercicio.sql

--- 11_ejercicio.sql
+++ 11_ejercicio.sql
@@ -3,3 +3,7 @@
 
 SELECT COUNT(*) AS total_detalles
 FROM detalle_ventas;
+
+-- Se cuenta el número total de registros en la tabla detalle_ventas.
+-- Resultado: existen 2,521 líneas de detalle de venta registradas.
+--2521
