7928a2124bf5e485f4da9b0d51d04c116f5fb205
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:30:26 -0500

    Update 09_ejercicio.sql

--- 09_ejercicio.sql
+++ 09_ejercicio.sql
@@ -3,6 +3,7 @@
 
 SELECT MAX(total_venta) AS mayor_venta
 FROM ventas;
-
+-- Se obtiene el valor máximo registrado en la columna total_venta.
+-- Resultado: se muestra el monto correspondiente a la venta más alta realizada.
 --"20200"
 
