b6bd51282bc1469f06707242ba958339461517fd
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:50:05 -0500

    Update 19_ejercicio.sql

--- 19_ejercicio.sql
+++ 19_ejercicio.sql
@@ -7,3 +7,6 @@ JOIN productos p ON d.id_producto = p.id_producto
 GROUP BY p.id_producto
 ORDER BY total_unidades DESC
 LIMIT 1;
+
+-- Se identifica el producto con mayor cantidad de unidades vendidas.
+-- Resultado: el producto más vendido es "Gafas de Noche Infra-ventiladas" con 274 unidades.
