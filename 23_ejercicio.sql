d054c76c2b528fd4935ccc072c0b6f3c1b042c4d
jsmontilla0105-wq <jsmontilla.0105@unicesmag.edu.co>
2026-02-26 20:51:47 -0500

    Update 23_ejercicio.sql

--- 23_ejercicio.sql
+++ 23_ejercicio.sql
@@ -8,3 +8,6 @@ JOIN productos p ON d.id_producto = p.id_producto
 GROUP BY p.id_producto
 ORDER BY ingreso_total DESC
 LIMIT 5;
+
+-- Se calculan los ingresos acumulados por producto.
+-- Resultado: se muestran los 5 productos con mayor ingreso total generado.
